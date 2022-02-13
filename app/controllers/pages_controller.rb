fdeadcbf92dc18b1d49ecbabbd8af35863fd7462
schennamon <mr.tolokneev@gmail.com>
2022-02-13 13:29:47 +0200

    Add control views, books and admins controller, routes modify

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def admin_panel
+  end
+
+  def categories_panel
+  end
+
+  def admin_panel
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  get 'admin_panel'=>'pages#admin_panel'
+  get 'categories_panel'=>'pages#categories_panel'
+  get 'books_panel'=>'pages#books_panel'
+  get 'authors_panel'=>'pages#authors_panel'
+  resources :books, except: [:index, :show]
   resources :categories, only: [:show]
   resources :authors, only: [:index, :show]
 end
